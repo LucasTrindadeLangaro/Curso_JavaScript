8c2f048e21587a5d4a5412e877c827b14cd849ea
Lucas Trindade Langaro <langarolucas@gmail.com>
2023-02-22 17:03:54 -0300

    var_let_const

---
+++ FUNDAMENTOS/var_let_Const.js
@@ -0,0 +1,15 @@
+var nome_variavel = 'valor da variavel'
+let nome_variavel_let = 'valor da variavel let'
+
+
+var a = 3
+let b = 4
+
+var a = 30
+b = 40
+
+a = 300
+b = 400
+
+const c = 5
+console.log('var =',a,'let =',b,'Const =',c)
